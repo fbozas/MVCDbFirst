c70351196045b7a0d5d40cdc3ec5f232df10c526
Panos Bozas <fbozas@hotmail.com>
2022-07-17 11:35:40 +0300

    Add CompanyModel, Add EmployeeDto, Add Automapper, Config EmployeeDto with Employee

---
+++ MVCDbFirst/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MVCDbFirst.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDbFirst.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Employee, EmployeeDto>();
+            Mapper.CreateMap<EmployeeDto, Employee>();
+        }
+    }
+}
---
+++ MVCDbFirst/Dtos/EmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDbFirst.Dtos
+{
+    public class EmployeeDto
+    {
+        public int ID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Depid { get; set; }
+        public double? Salary { get; set; }
+        public DateTime? Hiredate { get; set; }
+
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+    }
+}
