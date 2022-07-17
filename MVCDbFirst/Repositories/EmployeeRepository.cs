eddb3bebeee80de18f81b7be76cea2b228845de1
Panos Bozas <fbozas@hotmail.com>
2022-07-17 13:26:36 +0300

    Added EmployeeRepository

---
+++ MVCDbFirst/Repositories/EmployeeRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCDbFirst.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyContext _context;
+        public EmployeeRepository()
+        {
+            _context = new CompanyContext();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetById(int? id)
+        {
+            if (id == null)
+                throw new ArgumentNullException(nameof(id));
+
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+
+        public void Create(Employee employee)
+        {
+            if (employee == null)
+                throw new ArgumentNullException(nameof(employee));
+
+            _context.Employees.Add(employee);
+        }
+
+        public void Update(Employee employee)
+        {
+            if(employee == null)
+            {
+                throw new ArgumentNullException(nameof(employee));
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+        }
+
+        public void Delete(int? id)
+        {
+           if(id == null)
+           {
+                throw new ArgumentNullException(nameof(id));
+           }
+
+            Employee employee = _context.Employees.Find(id);
+            _context.Employees.Remove(employee);
+        }
+
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
---
+++ MVCDbFirst/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDbFirst.Repositories
+{
+    interface IEmployeeRepository : IDisposable
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int? id);
+
+        void Create(Employee employee);
+        void Update(Employee employee);
+        void Delete(int? id);
+
+    }
+}
